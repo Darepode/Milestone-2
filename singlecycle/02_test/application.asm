d506f320ec129640809f8d0e1f83f2cb6f0139e6
GiaHuyNguyen13 <giahuy130302003@gmail.com>
2024-10-22 13:11:51 +0700

    Application timer

---
+++ singlecycle/02_test/application.asm
@@ -0,0 +1,214 @@
+    .section .text
+    .globl _start
+
+_start:
+
+li x2, 0x7020  # Address for first HEX, each HEX add 1
+li x3, 10 # check value
+li x4, 9 # check value
+li x7, 5 
+li x20, 0 # HEX 0
+li x21, 0 # HEX 1
+li x22, 0 # HEX 2
+li x23, 0 # HEX 3
+li x24, 0 # HEX 4
+li x25, 0 # HEX 5
+li x26, 0 # HEX 6
+li x27, 0 # HEX 7
+
+sb x20, 0(x2) # HEX 0
+sb x21, 1(x2) # HEX 1
+sb x22, 2(x2) # HEX 2
+sb x23, 3(x2) # HEX 3
+sb x24, 4(x2) # HEX 4
+sb x25, 5(x2) # HEX 5
+sb x26, 6(x2) # HEX 6
+sb x27, 7(x2) # HEX 7
+
+here:
+mv x10, x20
+jal ra, seven_seg_decode
+sb x6, 0(x2)
+jal ra, delay_10ms
+addi x20, x20, 1
+bne x20, x3, here # if pass 9
+
+addi x20, x0, 0
+addi x21, x21, 1
+mv x10, x21
+jal ra, seven_seg_decode
+sb x6, 1(x2)
+bne x4, x21, here
+
+addi x21, x0, 0
+addi x22, x22, 1
+mv x10, x22
+jal ra, seven_seg_decode
+sb x6, 2(x2)
+bne x4, x22, here
+
+addi x22, x0, 0
+addi x23, x23, 1
+mv x10, x23
+jal ra, seven_seg_decode
+sb x6, 3(x2)
+bne x7, x23, here
+
+addi x23, x0, 0
+addi x24, x24, 1
+mv x10, x24
+jal ra, seven_seg_decode
+sb x6, 4(x2)
+bne x4, x24, here
+
+addi x24, x0, 0
+addi x25, x25, 1
+mv x10, x25
+jal ra, seven_seg_decode
+sb x6, 5(x2)
+bne x7, x25, here
+
+addi x25, x0, 0
+addi x26, x26, 1
+mv x10, x26
+jal ra, seven_seg_decode
+sb x6, 6(x2)
+bne x4, x26, here
+
+addi x26, x0, 0
+addi x27, x27, 1
+mv x10, x27
+jal ra, seven_seg_decode
+sb x6, 7(x2)
+bne x4, x27, here
+
+addi x27, x0, 0
+j here
+
+# Function: seven_seg_decode (exclude x10, x5, x6)
+# Input: a0 - Input value (0 to 15)
+# Output: Writes the corresponding bit pattern to a memory-mapped I/O register (0x10000000)
+seven_seg_decode:
+    # Check if a0 contains a valid value (0-F)
+    li      t0, 0           # Load immediate 0 into t0 for comparison
+    beq     a0, t0, display_0
+
+    li      t0, 1           # Check for 1
+    beq     a0, t0, display_1
+
+    li      t0, 2           # Check for 2
+    beq     a0, t0, display_2
+
+    li      t0, 3           # Check for 3
+    beq     a0, t0, display_3
+
+    li      t0, 4           # Check for 4
+    beq     a0, t0, display_4
+
+    li      t0, 5           # Check for 5
+    beq     a0, t0, display_5
+
+    li      t0, 6           # Check for 6
+    beq     a0, t0, display_6
+
+    li      t0, 7           # Check for 7
+    beq     a0, t0, display_7
+
+    li      t0, 8           # Check for 8
+    beq     a0, t0, display_8
+
+    li      t0, 9           # Check for 9
+    beq     a0, t0, display_9
+
+    li      t0, 10          # Check for A (10 in decimal)
+    beq     a0, t0, display_A
+
+    li      t0, 11          # Check for B (11 in decimal)
+    beq     a0, t0, display_B
+
+    li      t0, 12          # Check for C (12 in decimal)
+    beq     a0, t0, display_C
+
+    li      t0, 13          # Check for D (13 in decimal)
+    beq     a0, t0, display_D
+
+    li      t0, 14          # Check for E (14 in decimal)
+    beq     a0, t0, display_E
+
+    li      t0, 15          # Check for F (15 in decimal)
+    beq     a0, t0, display_F
+    
+    # If input is out of range, do nothing and return
+    jr ra
+
+# Define display patterns for each number (0-F)
+display_0:
+    li      t1, 0x40        # Bit pattern for '0'
+    jr ra
+
+display_1:
+    li      t1, 0x79        # Bit pattern for '1'
+    jr ra
+
+display_2:
+    li      t1, 0x24        # Bit pattern for '2'
+    jr ra
+
+display_3:
+    li      t1, 0x30        # Bit pattern for '3'
+    jr ra
+
+display_4:
+    li      t1, 0x19        # Bit pattern for '4'
+    jr ra
+
+display_5:
+    li      t1, 0x12        # Bit pattern for '5'
+    jr ra
+
+display_6:
+    li      t1, 0x02        # Bit pattern for '6'
+    jr ra
+
+display_7:
+    li      t1, 0x78        # Bit pattern for '7'
+    jr ra
+
+display_8:
+    li      t1, 0x00        # Bit pattern for '8'
+    jr ra
+
+display_9:
+    li      t1, 0x10        # Bit pattern for '9'
+    jr ra
+
+display_A:
+    li      t1, 0x08        # Bit pattern for 'A'
+    jr ra
+
+display_B:
+    li      t1, 0x03        # Bit pattern for 'B'
+    jr ra
+
+display_C:
+    li      t1, 0x46        # Bit pattern for 'C'
+    jr ra
+
+display_D:
+    li      t1, 0x21        # Bit pattern for 'D'
+    jr ra
+
+display_E:
+    li      t1, 0x06        # Bit pattern for 'E'
+    jr ra
+
+display_F:
+    li      t1, 0x0e        # Bit pattern for 'F'
+    jr ra
+
+delay_10ms:
+    li t0, 12499    # Load 50 million into t0 (1 second delay for 50 MHz clock)
+delay_loop:
+    addi t0, t0, -1      # Decrement the counter
+    bne t0, x0, delay_loop  # If t0 is not zero, branch back to delay_loop
+    jalr x0,x1,0                  # Return from the function
--- singlecycle/02_test/output.asm
+++ singlecycle/02_test/output.asm
@@ -6,122 +6,183 @@ Disassembly of section .text:
 
 00000000 <_start>:
    0:	00007137          	lui	sp,0x7
-   4:	02010113          	addi	sp,sp,32 # 7020 <delay_loop+0x6eec>
-   8:	01000593          	li	a1,16
+   4:	02010113          	addi	sp,sp,32 # 7020 <delay_loop+0x6df8>
+   8:	00a00193          	li	gp,10
+   c:	00900213          	li	tp,9
+  10:	00500393          	li	t2,5
+  14:	00000a13          	li	s4,0
+  18:	00000a93          	li	s5,0
+  1c:	00000b13          	li	s6,0
+  20:	00000b93          	li	s7,0
+  24:	00000c13          	li	s8,0
+  28:	00000c93          	li	s9,0
+  2c:	00000d13          	li	s10,0
+  30:	00000d93          	li	s11,0
+  34:	01410023          	sb	s4,0(sp)
+  38:	015100a3          	sb	s5,1(sp)
+  3c:	01610123          	sb	s6,2(sp)
+  40:	017101a3          	sb	s7,3(sp)
+  44:	01810223          	sb	s8,4(sp)
+  48:	019102a3          	sb	s9,5(sp)
+  4c:	01a10323          	sb	s10,6(sp)
+  50:	01b103a3          	sb	s11,7(sp)
 
-0000000c <here>:
-   c:	01c000ef          	jal	28 <seven_seg_decode>
-  10:	00610023          	sb	t1,0(sp)
-  14:	118000ef          	jal	12c <delay_1s>
-  18:	00150513          	addi	a0,a0,1
-  1c:	fea598e3          	bne	a1,a0,c <here>
-  20:	00000513          	li	a0,0
-  24:	fe9ff06f          	j	c <here>
+00000054 <here>:
+  54:	000a0513          	mv	a0,s4
+  58:	0c4000ef          	jal	11c <seven_seg_decode>
+  5c:	00610023          	sb	t1,0(sp)
+  60:	1c0000ef          	jal	220 <delay_10ms>
+  64:	001a0a13          	addi	s4,s4,1
+  68:	fe3a16e3          	bne	s4,gp,54 <here>
+  6c:	00000a13          	li	s4,0
+  70:	001a8a93          	addi	s5,s5,1
+  74:	000a8513          	mv	a0,s5
+  78:	0a4000ef          	jal	11c <seven_seg_decode>
+  7c:	006100a3          	sb	t1,1(sp)
+  80:	fd521ae3          	bne	tp,s5,54 <here>
+  84:	00000a93          	li	s5,0
+  88:	001b0b13          	addi	s6,s6,1
+  8c:	000b0513          	mv	a0,s6
+  90:	08c000ef          	jal	11c <seven_seg_decode>
+  94:	00610123          	sb	t1,2(sp)
+  98:	fb621ee3          	bne	tp,s6,54 <here>
+  9c:	00000b13          	li	s6,0
+  a0:	001b8b93          	addi	s7,s7,1
+  a4:	000b8513          	mv	a0,s7
+  a8:	074000ef          	jal	11c <seven_seg_decode>
+  ac:	006101a3          	sb	t1,3(sp)
+  b0:	fb7392e3          	bne	t2,s7,54 <here>
+  b4:	00000b93          	li	s7,0
+  b8:	001c0c13          	addi	s8,s8,1
+  bc:	000c0513          	mv	a0,s8
+  c0:	05c000ef          	jal	11c <seven_seg_decode>
+  c4:	00610223          	sb	t1,4(sp)
+  c8:	f98216e3          	bne	tp,s8,54 <here>
+  cc:	00000c13          	li	s8,0
+  d0:	001c8c93          	addi	s9,s9,1
+  d4:	000c8513          	mv	a0,s9
+  d8:	044000ef          	jal	11c <seven_seg_decode>
+  dc:	006102a3          	sb	t1,5(sp)
+  e0:	f7939ae3          	bne	t2,s9,54 <here>
+  e4:	00000c93          	li	s9,0
+  e8:	001d0d13          	addi	s10,s10,1
+  ec:	000d0513          	mv	a0,s10
+  f0:	02c000ef          	jal	11c <seven_seg_decode>
+  f4:	00610323          	sb	t1,6(sp)
+  f8:	f5a21ee3          	bne	tp,s10,54 <here>
+  fc:	00000d13          	li	s10,0
+ 100:	001d8d93          	addi	s11,s11,1
+ 104:	000d8513          	mv	a0,s11
+ 108:	014000ef          	jal	11c <seven_seg_decode>
+ 10c:	006103a3          	sb	t1,7(sp)
+ 110:	f5b212e3          	bne	tp,s11,54 <here>
+ 114:	00000d93          	li	s11,0
+ 118:	f3dff06f          	j	54 <here>
 
-00000028 <seven_seg_decode>:
-  28:	00000293          	li	t0,0
-  2c:	08550063          	beq	a0,t0,ac <display_0>
-  30:	00100293          	li	t0,1
-  34:	08550063          	beq	a0,t0,b4 <display_1>
-  38:	00200293          	li	t0,2
-  3c:	08550063          	beq	a0,t0,bc <display_2>
-  40:	00300293          	li	t0,3
-  44:	08550063          	beq	a0,t0,c4 <display_3>
-  48:	00400293          	li	t0,4
-  4c:	08550063          	beq	a0,t0,cc <display_4>
-  50:	00500293          	li	t0,5
-  54:	08550063          	beq	a0,t0,d4 <display_5>
-  58:	00600293          	li	t0,6
-  5c:	08550063          	beq	a0,t0,dc <display_6>
-  60:	00700293          	li	t0,7
-  64:	08550063          	beq	a0,t0,e4 <display_7>
-  68:	00800293          	li	t0,8
-  6c:	08550063          	beq	a0,t0,ec <display_8>
-  70:	00900293          	li	t0,9
-  74:	08550063          	beq	a0,t0,f4 <display_9>
-  78:	00a00293          	li	t0,10
-  7c:	08550063          	beq	a0,t0,fc <display_A>
-  80:	00b00293          	li	t0,11
-  84:	08550063          	beq	a0,t0,104 <display_B>
-  88:	00c00293          	li	t0,12
-  8c:	08550063          	beq	a0,t0,10c <display_C>
-  90:	00d00293          	li	t0,13
-  94:	08550063          	beq	a0,t0,114 <display_D>
-  98:	00e00293          	li	t0,14
-  9c:	08550063          	beq	a0,t0,11c <display_E>
-  a0:	00f00293          	li	t0,15
-  a4:	08550063          	beq	a0,t0,124 <display_F>
-  a8:	00008067          	ret
+0000011c <seven_seg_decode>:
+ 11c:	00000293          	li	t0,0
+ 120:	08550063          	beq	a0,t0,1a0 <display_0>
+ 124:	00100293          	li	t0,1
+ 128:	08550063          	beq	a0,t0,1a8 <display_1>
+ 12c:	00200293          	li	t0,2
+ 130:	08550063          	beq	a0,t0,1b0 <display_2>
+ 134:	00300293          	li	t0,3
+ 138:	08550063          	beq	a0,t0,1b8 <display_3>
+ 13c:	00400293          	li	t0,4
+ 140:	08550063          	beq	a0,t0,1c0 <display_4>
+ 144:	00500293          	li	t0,5
+ 148:	08550063          	beq	a0,t0,1c8 <display_5>
+ 14c:	00600293          	li	t0,6
+ 150:	08550063          	beq	a0,t0,1d0 <display_6>
+ 154:	00700293          	li	t0,7
+ 158:	08550063          	beq	a0,t0,1d8 <display_7>
+ 15c:	00800293          	li	t0,8
+ 160:	08550063          	beq	a0,t0,1e0 <display_8>
+ 164:	00900293          	li	t0,9
+ 168:	08550063          	beq	a0,t0,1e8 <display_9>
+ 16c:	00a00293          	li	t0,10
+ 170:	08550063          	beq	a0,t0,1f0 <display_A>
+ 174:	00b00293          	li	t0,11
+ 178:	08550063          	beq	a0,t0,1f8 <display_B>
+ 17c:	00c00293          	li	t0,12
+ 180:	08550063          	beq	a0,t0,200 <display_C>
+ 184:	00d00293          	li	t0,13
+ 188:	08550063          	beq	a0,t0,208 <display_D>
+ 18c:	00e00293          	li	t0,14
+ 190:	08550063          	beq	a0,t0,210 <display_E>
+ 194:	00f00293          	li	t0,15
+ 198:	08550063          	beq	a0,t0,218 <display_F>
+ 19c:	00008067          	ret
 
-000000ac <display_0>:
-  ac:	04000313          	li	t1,64
-  b0:	00008067          	ret
+000001a0 <display_0>:
+ 1a0:	04000313          	li	t1,64
+ 1a4:	00008067          	ret
 
-000000b4 <display_1>:
-  b4:	07900313          	li	t1,121
-  b8:	00008067          	ret
+000001a8 <display_1>:
+ 1a8:	07900313          	li	t1,121
+ 1ac:	00008067          	ret
 
-000000bc <display_2>:
-  bc:	02400313          	li	t1,36
-  c0:	00008067          	ret
+000001b0 <display_2>:
+ 1b0:	02400313          	li	t1,36
+ 1b4:	00008067          	ret
 
-000000c4 <display_3>:
-  c4:	03000313          	li	t1,48
-  c8:	00008067          	ret
+000001b8 <display_3>:
+ 1b8:	03000313          	li	t1,48
+ 1bc:	00008067          	ret
 
-000000cc <display_4>:
-  cc:	01900313          	li	t1,25
-  d0:	00008067          	ret
+000001c0 <display_4>:
+ 1c0:	01900313          	li	t1,25
+ 1c4:	00008067          	ret
 
-000000d4 <display_5>:
-  d4:	01200313          	li	t1,18
-  d8:	00008067          	ret
+000001c8 <display_5>:
+ 1c8:	01200313          	li	t1,18
+ 1cc:	00008067          	ret
 
-000000dc <display_6>:
-  dc:	00200313          	li	t1,2
-  e0:	00008067          	ret
+000001d0 <display_6>:
+ 1d0:	00200313          	li	t1,2
+ 1d4:	00008067          	ret
 
-000000e4 <display_7>:
-  e4:	07800313          	li	t1,120
-  e8:	00008067          	ret
+000001d8 <display_7>:
+ 1d8:	07800313          	li	t1,120
+ 1dc:	00008067          	ret
 
-000000ec <display_8>:
-  ec:	00000313          	li	t1,0
-  f0:	00008067          	ret
+000001e0 <display_8>:
+ 1e0:	00000313          	li	t1,0
+ 1e4:	00008067          	ret
 
-000000f4 <display_9>:
-  f4:	01000313          	li	t1,16
-  f8:	00008067          	ret
+000001e8 <display_9>:
+ 1e8:	01000313          	li	t1,16
+ 1ec:	00008067          	ret
 
-000000fc <display_A>:
-  fc:	00800313          	li	t1,8
- 100:	00008067          	ret
+000001f0 <display_A>:
+ 1f0:	00800313          	li	t1,8
+ 1f4:	00008067          	ret
 
-00000104 <display_B>:
- 104:	00300313          	li	t1,3
- 108:	00008067          	ret
+000001f8 <display_B>:
+ 1f8:	00300313          	li	t1,3
+ 1fc:	00008067          	ret
 
-0000010c <display_C>:
- 10c:	04600313          	li	t1,70
- 110:	00008067          	ret
+00000200 <display_C>:
+ 200:	04600313          	li	t1,70
+ 204:	00008067          	ret
 
-00000114 <display_D>:
- 114:	02100313          	li	t1,33
- 118:	00008067          	ret
+00000208 <display_D>:
+ 208:	02100313          	li	t1,33
+ 20c:	00008067          	ret
 
-0000011c <display_E>:
- 11c:	00600313          	li	t1,6
- 120:	00008067          	ret
+00000210 <display_E>:
+ 210:	00600313          	li	t1,6
+ 214:	00008067          	ret
 
-00000124 <display_F>:
- 124:	00e00313          	li	t1,14
- 128:	00008067          	ret
+00000218 <display_F>:
+ 218:	00e00313          	li	t1,14
+ 21c:	00008067          	ret
 
-0000012c <delay_1s>:
- 12c:	017d82b7          	lui	t0,0x17d8
- 130:	84028293          	addi	t0,t0,-1984 # 17d7840 <delay_loop+0x17d770c>
+00000220 <delay_10ms>:
+ 220:	000032b7          	lui	t0,0x3
+ 224:	0d328293          	addi	t0,t0,211 # 30d3 <delay_loop+0x2eab>
 
-00000134 <delay_loop>:
- 134:	fff28293          	addi	t0,t0,-1
- 138:	fe029ee3          	bnez	t0,134 <delay_loop>
- 13c:	00008067          	ret
+00000228 <delay_loop>:
+ 228:	fff28293          	addi	t0,t0,-1
+ 22c:	fe029ee3          	bnez	t0,228 <delay_loop>
+ 230:	00008067          	ret
